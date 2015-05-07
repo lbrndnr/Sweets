54bd84b53b11adf135ee005ee9fbe3fa6472e0e4
Laurin Brandner <mail@laurinbrandner.ch>
2015-05-07 07:55:30 +0200

    Initial Commit

---
+++ Sweets/Sweets.h
@@ -0,0 +1,19 @@
+//
+//  Sweets.h
+//  Sweets
+//
+//  Created by Laurin Brandner on 07/05/15.
+//  Copyright (c) 2015 Laurin Brandner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sweets.
+FOUNDATION_EXPORT double SweetsVersionNumber;
+
+//! Project version string for Sweets.
+FOUNDATION_EXPORT const unsigned char SweetsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sweets/PublicHeader.h>
+
+
